937a9991e82b228c7295d6946afb8b536143c844
arjunv <arjunv.nxtgen.com>
2025-07-15 12:51:52 +0530

    added profile icons to the code

--- my-vite-app/src/Topbar.jsx
+++ my-vite-app/src/Topbar.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import useApi from "./hooks/useSwr";
 import { useNavigate, useParams } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
+import { jwtDecode } from "jwt-decode";
 
 export const Topbar = () => {
   const nav = useNavigate();
+  const token = localStorage.getItem("token");
+  const decoded = jwtDecode(token);
   const { id } = useParams();
   const { data } = useApi(`/users/${id}`);
-  const token = localStorage.getItem("token");
 
   const LogOut = () => {
     if (!token) {
@@ -19,16 +21,34 @@ export const Topbar = () => {
     nav("/signup");
     toast.success("Logout successful", { position: "bottom-center" });
   };
+
   return (
     <>
-      <div className="flex  gap-5  items-center justify-end pr-4">
+      <div className="flex  gap-5  items-center justify-end pr-4 mt-5">
         <p>{data?.details?.name}</p>
-        <button
-          onClick={LogOut}
-          className="cursor-pointer font-bold bg-blue-500 w-22 rounded-md p-2 text-white"
-        >
-          Sign Out
-        </button>
+        <div className="flex gap-2">
+          <div
+            className="rounded-4xl w-10 h-10 bg-amber-700 cursor-pointer"
+            onClick={() => nav(`/home/user/${decoded?._id}`)}
+          >
+            {data?.details?.avatar ? (
+              <img src={data?.details?.avatar} alt="" />
+            ) : (
+              <p className="font-medium text-white flex items-center justify-center mt-2">
+                {decoded?.name
+                  ? decoded?.name.charAt(0).toUpperCase() +
+                    decoded?.name.charAt(1).toLowerCase()
+                  : ""}
+              </p>
+            )}
+          </div>
+          <button
+            onClick={LogOut}
+            className="cursor-pointer font-bold bg-black w-22 rounded-md p-2 text-white"
+          >
+            Sign Out
+          </button>
+        </div>
       </div>
       <Toaster />
     </>
